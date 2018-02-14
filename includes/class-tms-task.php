d6641150341fdad2b2e848bc00413df7692016c2
Shantanu <shantanu2846@gmail.com>
2018-02-14 15:22:15 +0530

    Add properties to class-tms-task

--- includes/class-tms-task.php
+++ includes/class-tms-task.php
@@ -1,1 +1,64 @@
 <?php
+/**
+ * Task Management System
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) exit();
+
+if ( !class_exists( 'TMS_Task' ) ) {
+	
+	/**
+	 * TMS_Task
+	 *
+	 * @since 0.0.1
+	 */
+	class TMS_Task {
+		
+		/**
+		 * Unique identifier for the task
+		 * 
+		 * @since 0.0.1
+		 * 
+		 * @var  int 
+		 */
+		private $ID;
+		
+		/**
+		 * Description of the task
+		 * 
+		 * @since 0.0.1
+		 * 
+		 * @var  string 
+		 */
+		private $task_description;
+		
+		/**
+		 * Status of the task ( complete or incomplete )
+		 * 
+		 * @since 0.0.1
+		 * 
+		 * @var  boolean 
+		 */
+		private $task_status;
+		
+		/**
+		 * List of all the subtasks
+		 * 
+		 * @since 0.0.1
+		 * 
+		 * @var  array 
+		 */
+		private $subtasks;
+		
+		
+		public function __construct() {
+			
+			$this->subtasks = array();
+		}
+	}// class TMS_Task
+	
+}// class_exists
